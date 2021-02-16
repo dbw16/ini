d0e3766284129f7a6b503e6e7d8d4c1d864adacc
David <dawillia@tcd.ie>
2021-02-16 18:00:34 +0000

    some info

--- ini.py
+++ ini.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 import sys
 import os.path
+"""
+utility function, for print objects that are in the first list and not in the second,
+should be added to bin and can be called in two ways with file or stdin
+Examples 
+ini "1 2 3" "4"
+ini file_1 file_2 
+ini "$(cat file_1 | cut -d "-" -f 1)" "$(cat 2)"
+"""
 
 if len(sys.argv) != 3:
     print("needs two arguments")
